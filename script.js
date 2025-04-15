e7f809926634e7d76b0a92e059b76976b3702561
wellingtonbertoldo <146102304+wellingtonbertoldo@users.noreply.github.com>
2025-04-15 11:28:24 -0300

    tom

--- script.js
+++ script.js
@@ -7,4 +7,7 @@ document.addEventListener('DOMContentLoaded', function(){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 })
